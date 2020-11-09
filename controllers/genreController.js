b2a4239d4e935fb75598050822c06920514f8db4
HootDunk <58009556+HootDunk@users.noreply.github.com>
2020-11-08 19:55:07 -0600

    lesson learned, dont forget the '-' in ejs!

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,7 +1,7 @@
 var Genre = require('../models/genre');
 var Book = require('../models/book');
 var async = require('async');
-var mongoose = require('mongoose');
+
 
 // Display list of all Genre.
 exports.genre_list = function(req, res, next) {
@@ -16,8 +16,7 @@ exports.genre_list = function(req, res, next) {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = function(req, res, next) {
-
-    // var id = mongoose.Types.ObjectId(req.params.id);  
+ 
     async.parallel({
         genre: function(callback) {
 
